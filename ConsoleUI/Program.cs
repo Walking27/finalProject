927db8763b8df34e98a0f2692fae4b995841ab39
Walking27 <76839270+Walking27@users.noreply.github.com>
2021-02-20 18:32:53 +0300

    ...

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrate;
+using DataAccess.Concrate.InMemory;
+using Entities.Concrate;
+using System;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var Car in carManager.GetAll())
+            {
+                Console.WriteLine(Car.Id);
+            }
+        }
+    }
+}
+
---
+++ DataAccess/Concrate/EntityFramwork/EfCarDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrate.EntityFramwork
+{
+    public class EfCarDal : EfEntityReposiroryBase<Car, CarRentalContext>,ICarDal
+    {
+        
+    }
+}

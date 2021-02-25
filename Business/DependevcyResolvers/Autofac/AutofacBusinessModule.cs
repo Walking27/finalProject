c2f3e8db1d677141eb553f8d1c0f3e7077d47e9a
Walking27 <76839270+Walking27@users.noreply.github.com>
2021-02-25 13:11:47 +0300

    24 Şubat Gün Sonu

--- Business/DependevcyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependevcyResolvers/Autofac/AutofacBusinessModule.cs
@@ -15,6 +15,13 @@ namespace Business.DependevcyResolvers.Autofac
         {
             builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
             builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
         }
     }
 }

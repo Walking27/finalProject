711c01afcfb7de0f8d2f7f9398099e259db15ac6
Walking27 <76839270+Walking27@users.noreply.github.com>
2021-03-01 16:49:59 +0300

    27 Şubat

---
+++ Core/Utilites/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Comcrate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilites.Security.JWT
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+    }
+}

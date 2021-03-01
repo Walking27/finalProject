69d0e7cd04df010f0963d40f68ed56907f41af92
Walking27 <76839270+Walking27@users.noreply.github.com>
2021-03-01 14:53:46 +0300

    .

---
+++ Core/Entities/Concrate/OperationClaim.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities.Comcrate
+{
+    public class OperationClaim:IEntity
+    {
+        public int Id { get; set;}
+        public int Name { get; set; }
+    }
+}
---
+++ Core/Utilites/Security/Hasing/HasingHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilites.Security.Hasing
+{
+    class HasingHelper
+    {
+    }
+}
